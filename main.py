3ea5c5551861e8398669f811315d149a593b795b
poojathakur-23 <76743633+poojathakur-23@users.noreply.github.com>
2025-07-02 09:41:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
 st.title("Stremlit Application")
-
 st.write("creating a simple web application using streamlit")
+name=st.text_input("Enter your name")
+if st.button("submit"):
+  st.write("hello", name, "welcome to strwamlit")
